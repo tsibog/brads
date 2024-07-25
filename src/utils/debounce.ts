1148064aa585118b7d42e7b3876fbc31023a9157
Daniel Andersson <dandersson23@gmail.com>
2024-07-25 14:36:30 +0200

    feat: add debounce

---
+++ src/utils/debounce.ts
@@ -0,0 +1,15 @@
+function debounce<T extends (...args: unknown[]) => Promise<void>>(
+	func: T,
+	wait: number = 500
+): (...args: Parameters<T>) => void {
+	let timeout: ReturnType<typeof setTimeout> | null = null;
+
+	return (...args: Parameters<T>) => {
+		if (timeout) clearTimeout(timeout);
+
+		timeout = setTimeout(() => {
+			func(...args);
+		}, wait);
+	};
+}
+export default debounce;
