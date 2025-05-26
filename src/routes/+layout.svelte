41961951a9fc6aecd49eeefa852ebeac90193f40
Daniel Andersson <dandersson23@gmail.com>
2025-05-26 13:02:49 +0200

    feat: add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
-<div class="bg-brads-yellow-light min-h-screen">
+<div class="bg-brads-yellow-light min-h-screen font-londrina">
 	<slot />
 </div>
